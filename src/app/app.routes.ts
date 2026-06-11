48a0883a5548405034432f08404d9d587c12d454
M-Diouf-dev <diouftapha2503@gmail.com>
2026-06-11 08:54:24 +0000

    l'ajout du navBar au niveau du produit html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { FormulaireAjout } from './components/formulaire_ajout/formulaire_ajout'
 import { Produits } from './components/produits/produits';
 import { ProduitDetails } from './components/produit-details/produit-details';
 import { ModifierProduits } from './components/modifier-produits/modifier-produits';
+import { NavBar } from './components/nav-bar/nav-bar';
 
 export const routes: Routes = [
     {path:'',redirectTo:'produits',pathMatch:'full'},
     {path:'produits',component:Produits},
     {path:'ajouter',component:FormulaireAjout}, 
     {path:'details/:id',component:ProduitDetails},
-    {path:'modifier/:id',component:ModifierProduits}
+    {path:'modifier/:id',component:ModifierProduits},
+    {path:'voila',component:NavBar}
 ];
 
 
--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
